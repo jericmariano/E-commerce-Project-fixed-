d875244ceb2c98ae7442d8d63bb8e4df31ff7698
Jeric <marianovjeric@gmail.com>
2022-12-04 21:46:45 -0800

    fixed dynamic sale prices

--- books.js
+++ books.js
@@ -27,7 +27,7 @@ function renderBooks(filter) {
         ${ratingsHTML(book.rating)}
       </div>
       <div class="book__price">
-        <span>$${book.originalPrice.toFixed(2)}</span>
+        ${priceHTML(book.originalPrice, book.salePrice)}
       </div>
     </div>`
     }).join('')
@@ -35,6 +35,15 @@ function renderBooks(filter) {
     booksWrapper.innerHTML = booksHtml
     // booksWrapper.innerHTML = 
    
+  }
+
+  function priceHTML(originalPrice, salePrice) {
+    if (!salePrice) {
+        return `$${originalPrice.toFixed(2)}`
+    }
+    else {
+    return `<span class="book__price--normal">$${originalPrice.toFixed(2)}</span> $${salePrice.toFixed(2)}`
+    }
   }
 
   function ratingsHTML(rating) {
