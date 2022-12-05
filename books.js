fe5782576275bbfda908263d29264568bec1f612
Jeric <marianovjeric@gmail.com>
2022-12-04 21:31:20 -0800

    dynamic stars rating filter

--- books.js
+++ books.js
@@ -13,7 +13,6 @@ function renderBooks(filter) {
       books.sort((a, b) => b.rating - a.rating)
     }
   
-  
     const booksHtml = books.map(book => {
       return `<div class="book">
       <figure class="book__img--wrapper">
@@ -25,11 +24,7 @@ function renderBooks(filter) {
       </figure>
       <div class="book__title">${book.title}</div>
       <div class="book__ratings">
-        <i class="fas fa-star"></i>
-        <i class="fas fa-star"></i>
-        <i class="fas fa-star"></i>
-        <i class="fas fa-star"></i>
-        <i class="fas fa-star-half-alt"></i>
+        ${ratingsHTML(book.rating)}
       </div>
       <div class="book__price">
         <span>$${book.originalPrice.toFixed(2)}</span>
@@ -40,6 +35,20 @@ function renderBooks(filter) {
     booksWrapper.innerHTML = booksHtml
     // booksWrapper.innerHTML = 
    
+  }
+
+  function ratingsHTML(rating) {
+    let ratingHTML = ''
+
+    for (let i = 0; i < Math.floor(rating); ++i) {
+        ratingHTML += '<i class="fas fa-star"></i>'
+    }
+
+    if (!Number.isInteger(rating)) {
+        ratingHTML += '<i class="fas fa-star-half-alt"></i>'
+    }
+    
+    return ratingHTML 
   }
   
   function filterBooks(event) {
